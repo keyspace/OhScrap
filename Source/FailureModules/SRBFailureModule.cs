7080c638564ac5f1b577bf7ef815a893a80dc8bf
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-12-28 21:54:40 -0600

    Update SRBFailureModule.cs

--- Source/FailureModules/SRBFailureModule.cs
+++ Source/FailureModules/SRBFailureModule.cs
@@ -5,6 +5,7 @@ using System.Text;
 using UnityEngine;
 using System.Runtime.CompilerServices;
 using KSP.UI.Screens;
+using KSP.Localization;
 
 namespace OhScrap
 {
@@ -17,9 +18,10 @@ namespace OhScrap
         //They will however suppress the messages until the player tries to launch.
         protected override void Overrides()
         {
-            Fields["displayChance"].guiName = "Chance of SRB Failure";
-            Fields["safetyRating"].guiName = "SRB Safety Rating";
-            failureType = "ignition failure";
+            Fields["displayChance"].guiName = Localizer.Format("#OHS-srb-00");
+            Fields["safetyRating"].guiName = Localizer.Format("#OHS-srb-01");
+            failureType = Localizer.Format("#OHS-srb-02");
+
             engine = part.FindModuleImplementing<ModuleEngines>();
             isSRB = true;
         }
@@ -42,11 +44,13 @@ namespace OhScrap
             if (!message)
             {
                 if (vessel.vesselType != VesselType.Debris)
-                    ScreenMessages.PostScreenMessage(part.partInfo.title + " has failed to ignite");
+                    ScreenMessages.PostScreenMessage(Localizer.Format("#OHS-srb-03", part.partInfo.title));
+                    //ScreenMessages.PostScreenMessage(part.partInfo.title + " has failed to ignite");
                 StringBuilder msg = new StringBuilder();
                 msg.AppendLine(part.vessel.vesselName);
                 msg.AppendLine("");
-                msg.AppendLine(part.partInfo.title + " has suffered an " + failureType);
+                msg.AppendLine(Localizer.Format("#OHS-srb-04", part.partInfo.title, failureType));
+                //msg.AppendLine(part.partInfo.title + " has suffered an " + failureType);
                 msg.AppendLine("");
                 MessageSystem.Message m = new MessageSystem.Message("OhScrap", msg.ToString(),
                     MessageSystemButton.MessageButtonColor.ORANGE, MessageSystemButton.ButtonIcons.ALERT);
@@ -63,7 +67,7 @@ namespace OhScrap
         //SRBs cant be repaired.
         public override void RepairPart()
         {
-            ScreenMessages.PostScreenMessage("Igniting an SRB manually doesn't seem like a good idea");
+            ScreenMessages.PostScreenMessage(Localizer.Format("#OHS-srb-05"));
             ModuleUPFMEvents UPFM = part.FindModuleImplementing<ModuleUPFMEvents>();
             UPFM.customFailureEvent = true;
         }
