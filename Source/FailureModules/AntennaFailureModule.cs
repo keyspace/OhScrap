9477466c240138df24de3e8a7c1b850c1f1b6a61
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-12-28 19:53:12 -0600

    Update AntennaFailureModule.cs

--- Source/FailureModules/AntennaFailureModule.cs
+++ Source/FailureModules/AntennaFailureModule.cs
@@ -41,7 +41,7 @@ namespace OhScrap
                 if (deployableAntenna.deployState != ModuleDeployablePart.DeployState.EXTENDED) return false;
             }
             if (antenna == null) return false;
-            return (HighLogic.CurrentGame.Parameters.CustomParams<UPFMSettings>().AntennaFailureModuleAllowed
+            return (HighLogic.CurrentGame.Parameters.CustomParams<Settings>().AntennaFailureModuleAllowed
                     && CommNet.CommNetScenario.CommNetEnabled
                     && antenna.CommType != 0); // Not an internal antenna. Command pods without external antennas should not get an antenna failure.
         }
